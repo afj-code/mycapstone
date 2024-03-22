6e4d89c93392e37ed5a030306ec08c083e40ae8d
afj-code <bakarejuwon112@gmail.com>
2024-03-22 13:30:27 -0400

    fix

--- environment_setup/setup.sh
+++ environment_setup/setup.sh
@@ -12,6 +12,7 @@ sudo apt install python-pip -y
 sudo apt install ansible -y
 sudo apt install awscli -y
 sudo apt update -y
+sudo apt-get install sshpass -y
 
 #Run ansible script
 cd ~/mycapstone/ansible
